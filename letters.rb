ad57bf3cb92bd0e2ba66cc43b89040cdd249f5df
Tato Panchulidze <tpanchulidze@unisens.ge>
2022-03-28 16:38:58 +0400

    letters

---
+++ letters.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# letters
+
+class Letters
+  def create_vowel(string)
+    @vowel = { 'a' => 0, 'e' => 0, 'i' => 0, 'o' => 0, 'u' => 0 }
+    string = string.downcase.gsub(/[^aeiuo]/, '')
+    string.each_char do |i|
+      @vowel[i] += 1
+    end
+    @vowel
+  end
+end
+
+p Letters.new.create_vowel('ajsdwideeuuookhll')
