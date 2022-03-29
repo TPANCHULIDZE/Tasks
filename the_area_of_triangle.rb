50bef2a32568a5b15a1a03dfbf45cddc4b95baa9
Tato Panchulidze <tpanchulidze@unisens.ge>
2022-03-29 10:37:50 +0400

    correct code in rec_triangle branch

--- the_area_of_triangle.rb
+++ the_area_of_triangle.rb
@@ -1,7 +1,18 @@
+# calculate triangle area
+
 class Triangle
-	def area(a, h)
-		return (a * h) / 2
-	end
+  CONST = 0.5
+  def area(base, height)
+    @base, @height = base, height
+    check_errors
+    base * height * CONST
+  end
+
+  def check_errors
+    unless (@base.is_a? Float) && (@height.is_a? Float)
+      raise ArgumentError.new('base and height have to be Float')
+    end
+  end
 end
 
 base = gets.chomp.to_f
