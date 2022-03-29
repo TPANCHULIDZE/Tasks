faf444d65fd7f0b92c47df112e209e6cb854b0e6
Tato Panchulidze <tpanchulidze@unisens.ge>
2022-03-29 10:30:08 +0400

    correct code in height branch

--- ideal_weight.rb
+++ ideal_weight.rb
@@ -1,13 +1,23 @@
+# calculate ideal weight
+
 class Human
-	def ideal_weight(name, height)
-		if height - 110 >= 0
-			return "Hello #{name}, your height is #{height} and your optimal weight is #{height - 110}"
-		else
-			return "Hello #{name}, Your weight is already optimal"
-		end
-	end
+  CONST = 110
+  def ideal_weight(name, height)
+    @name, @height = name, height
+    weight = height - CONST
+    check_errors
+    unless weight.negative?
+      "Hello #{name}, your height is #{height} and your optimal weight is #{weight}"
+    else
+      "Hello #{name}, Your weight is already optimal"
+    end
+  end
 
-
+  def check_errors
+    unless @height.is_a? Float
+      raise ArgumentError.new('height have to be Float')
+    end
+  end
 end
 
 human = Human.new
