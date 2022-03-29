75e9997c70f7d64d5db0a8800d1d8ef11b1c9424
Tato Panchulidze <tpanchulidze@unisens.ge>
2022-03-29 11:04:11 +0400

    correct code in increment branch

--- quadratic.rb
+++ quadratic.rb
@@ -1,39 +1,49 @@
-# frozen_string_literal: true
+# quadratic equation, calculate roots and discriminant
 
 class Quadratic
-  def equation(a, b, c)
-    @a = a
-    @b = b
-    @c = c
+  CONST = 4
+  CONST1 = 2
+  def equation(first, second, third)
+    @first, @second, @third = first, second, third
+    check_errors
+    answer
+  end
 
-    @disc = find_disc
-
-    if @disc.negative?
-      "Discriminant is #{@disc}, No Roots"
-    else
-      root1 = find_root(@disc, '+')
-      root2 = find_root(@disc, '-')
-      if root1 == root2
-        "Discriminant is #{@disc} and Root is #{root1}"
-      else
-        "Discriminant is #{@disc}, and Roots are #{root1} and #{root2}"
-      end
+  def answer
+    disc = find_disc
+    case 
+    when disc.negative?
+      "Discriminant is #{disc}, No Roots"
+    when disc.positive?
+      root1 = find_root(disc, '+')
+      root2 = find_root(disc, '-')
+      "Discriminant is #{disc}, and Roots are #{root1} and #{root2}"
+    when disc.zero?
+      root = find_root(disc, '+')
+      "Discriminant is #{disc} and Root is #{root}"
     end
   end
 
   def find_disc
-    @b**2 - 4 * @a * @c
+    @second**2 - CONST * @first * @third
   end
 
   def find_root(disc, operation)
-    -@b.send(operation, Math.sqrt(disc)) / (2 * @a)
+    -@second.send(operation, Math.sqrt(disc)) / (CONST1 * @first)
+  end
+
+  def check_errors
+    unless (@first.is_a? Float) && (@second.is_a? Float) && (@third.is_a? Float)
+      raise ArgumentError.new('Every coefficient have to be Float')
+    end
   end
 end
 
+
+
+first = gets.chomp.to_f
+second = gets.chomp.to_f
+third = gets.chomp.to_f
+
 quadratic = Quadratic.new
-
-a = gets.chomp.to_f
-b = gets.chomp.to_f
-c = gets.chomp.to_f
-
-quadratic.equation(a, b, c)
+puts quadratic.equation(first, second, third)
