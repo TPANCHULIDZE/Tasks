d6355aea46b4b95c2dd938098f73f171f56dec56
Tato Panchulidze <tpanchulidze@unisens.ge>
2022-04-01 09:30:42 +0400

    correct code

--- the_area_of_triangle.rb
+++ the_area_of_triangle.rb
@@ -7,15 +7,19 @@ class Triangle
     @base, @height = base, height
   end
 
-  def calculate_area(base, height)
+  def calculate_area
     check_errors
-    base * height * HALF
+    @base * @height * HALF
   end
 
   def check_errors
-    unless (@base.is_a? Float) && (@height.is_a? Float)
+    unless (@base.is_a? Float) && (@height.is_a? Float) && is_not_zero?
       raise ArgumentError.new('base and height have to be Float')
     end
+  end
+
+  def is_not_zero?
+    !(@base.zero? && @height.zero?)
   end
 end
 
@@ -25,3 +29,5 @@ height = gets.chomp.to_f
 triangle = Triangle.new(base, height)
 
 puts triangle.calculate_area
+
+
