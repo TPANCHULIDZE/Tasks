5255f31a7965568e25cff834b3aebc3eb047e04a
Tato Panchulidze <tpanchulidze@unisens.ge>
2022-04-01 09:21:12 +0400

    correct code

--- ideal_weight.rb
+++ ideal_weight.rb
@@ -6,21 +6,20 @@ class Human
   def initialize(name, height)
     @name = name
     @height = height
-    
   end
 
   def ideal_weight
     check_errors
-    weight = height - IDEAL_WEIGHT
+    weight = @height - IDEAL_WEIGHT
     unless weight.negative?
-      "Hello #{name}, your height is #{height} and your optimal weight is #{weight}"
+      "Hello #{@name}, your height is #{@height} and your optimal weight is #{weight}"
     else
-      "Hello #{name}, Your weight is already optimal"
+      "Hello #{@name}, Your weight is already optimal"
     end
   end
 
   def check_errors
-    unless @height.is_a? Float
+    unless @height.is_a? Float 
       raise ArgumentError.new('height have to be Float')
     end
   end
