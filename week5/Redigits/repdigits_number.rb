4a66fcd088b093e702e8809b79ef39c3b2d65a43
Tato Panchulidze <tpanchulidze@unisens.ge>
2022-04-11 15:06:02 +0400

    find redigit number

---
+++ week5/Redigits/repdigits_number.rb
@@ -0,0 +1,33 @@
+# find out if number is a repdigits number
+
+class RepdigitsNumber
+  class IntegerError < StandardError; end
+
+  def initialize(number)
+    @number = number
+  end
+
+  def call
+    check_validate
+    is_repdigit?
+  rescue IntegerError => e
+    warn e
+  end
+
+  private
+
+  def is_repdigit?
+    @number.split('').uniq.size == 1
+  end
+
+  def check_validate
+    return if (@number.to_i.to_s == @number && @number.to_i.positive?)
+
+    raise IntegerError, "input value must be positive number"
+  end
+end
+
+puts "please enter positive integer"
+number = gets.chomp
+
+puts RepdigitsNumber.new(number).call
