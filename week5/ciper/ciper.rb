3477f94013409da5da7dbc519039a226fa449a45
Tato Panchulidze <tpanchulidze@unisens.ge>
2022-04-12 14:58:28 +0400

    reccorect code

--- week5/ciper/ciper.rb
+++ week5/ciper/ciper.rb
@@ -1,16 +1,22 @@
 # converting string by caesar ciper
   
-LOWER_ALFABET = ('a'..'z').to_a
-UPPER_ALFABET = ('A'..'Z').to_a
+LOWER_ALPHABET = ('a'..'z').to_a
+UPPER_ALPHABET = ('A'..'Z').to_a
+ALPHABET_SIZE = 26
 
 class CaesarCiper
+  class IntegerError < StandardError; end
+
   def initialize(ciper_string, ciper_number)
     @ciper_string = ciper_string
     @ciper_number = ciper_number
   end
 
   def call 
+    check_validate
     converting_string
+  rescue IntegerError => e
+    e.message
   end
 
   private
@@ -18,10 +24,10 @@ class CaesarCiper
   def converting_string
     @answer = ""
     @ciper_string.each_char do |char|
-      if LOWER_ALFABET.include? char
-        @answer += find_new_index(LOWER_ALFABET, char)
-      elsif UPPER_ALFABET.include? char
-        @answer += find_new_index(UPPER_ALFABET, char)
+      if LOWER_ALPHABET.include? char
+        @answer += find_new_index(LOWER_ALPHABET, char)
+      elsif UPPER_ALPHABET.include? char
+        @answer += find_new_index(UPPER_ALPHABET, char)
       else
         @answer += char
       end
@@ -30,8 +36,18 @@ class CaesarCiper
   end
 
   def find_new_index(alfabet, char)
-    alfabet[(alfabet.index(char) + @ciper_number) % 26]
+    alfabet[(alfabet.index(char) + @ciper_number.to_i) % ALPHABET_SIZE]
+  end
+
+  def check_validate
+    return if @ciper_number.to_i.to_s == @ciper_number
+
+    raise IntegerError, "ciper number must be integer"
   end
 end
 
-#puts CaesarCiper.new("What a string!",3).call
+# string = gets.chomp
+# number = gets.chomp
+# puts CaesarCiper.new(string, number).call
+
+
---
+++ week5/ciper/spec/ciper_tester.rb
@@ -0,0 +1,10 @@
+#check caesar ciper code
+
+require '../ciper'
+require 'rspec/autorun'
+require 'faker'
+
+describe CaesarCiper do
+  
+
+end
