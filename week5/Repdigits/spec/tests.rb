2ce518d4029cbee448bde85236068cbbfd37ae59
Tato Panchulidze <tpanchulidze@unisens.ge>
2022-04-12 15:02:32 +0400

    correct tester

--- week5/Repdigits/spec/tests.rb
+++ week5/Repdigits/spec/tests.rb
@@ -9,6 +9,7 @@ SMALL_TESTS_NUMBER = 10
 MIN_ALPHA = 10
 MIN_NUMERIC = 10
 NUMBER = 1000
+ERROR_MESSAGE = "input value must be positive number"
 
 describe RepdigitsNumber do 
   faker = Faker::Number.within(range: ONE_NUMBER_RANGE)
@@ -16,12 +17,10 @@ describe RepdigitsNumber do
   string_faker = Faker::Alphanumeric.alphanumeric(number: NUMBER, min_alpha: MIN_ALPHA)
 
   TESTS_NUMBER.times do 
-    it "check that number is repdigit number" do 
-      expect(RepdigitsNumber.new(faker.to_s * product_faker).call).to equal true
-    end
-  end
+    faker = Faker::Number.within(range: ONE_NUMBER_RANGE)
+    product_faker = Faker::Number.within(range:PRODUCT_RANGE)
+    string_faker = Faker::Alphanumeric.alphanumeric(number: NUMBER, min_alpha: MIN_ALPHA)
 
-  TESTS_NUMBER.times do
     number1 = (faker * product_faker).to_s
     number2 = (faker * product_faker).to_s
     number = ''
@@ -30,6 +29,11 @@ describe RepdigitsNumber do
     else
       number = number1 + number2 + '0'
     end
+
+    it "check that number is repdigit number" do 
+      expect(RepdigitsNumber.new(faker.to_s * product_faker).call).to equal true
+    end
+
     it "check that number is not repdigits" do 
       expect(RepdigitsNumber.new(number).call).to equal false
     end
@@ -37,19 +41,17 @@ describe RepdigitsNumber do
 
   SMALL_TESTS_NUMBER.times do 
     it "raise error when number is not positive integer" do 
-      expect(RepdigitsNumber.new(string_faker).call).eql? "input value must be positive number"
+      expect(RepdigitsNumber.new(string_faker).call).eql? ERROR_MESSAGE
     end
-  end
 
-  SMALL_TESTS_NUMBER.times do
     it "raise error when number is empty string" do 
       negative_num = '-' + faker.to_s * product_faker
-      expect(RepdigitsNumber.new(negative_num).call).eql? "input value must be positive number"
+      expect(RepdigitsNumber.new(negative_num).call).eql? ERROR_MESSAGE
     end
   end
 
   it "raise error when number is empty string" do 
-    expect(RepdigitsNumber.new('').call).eql? "input value must be positive number"
+    expect(RepdigitsNumber.new('').call).eql? ERROR_MESSAGE
   end
 end
 
