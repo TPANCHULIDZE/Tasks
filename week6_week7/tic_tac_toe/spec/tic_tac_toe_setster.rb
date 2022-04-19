7092f18f60fd31defdcc9d85cc3ccc08b300bb29
Tato Panchulidze <tpanchulidze@unisens.ge>
2022-04-19 17:18:58 +0400

    add tests for tic tac toe game

--- week6_week7/tic_tac_toe/spec/tic_tac_toe_setster.rb
+++ week6_week7/tic_tac_toe/spec/tic_tac_toe_setster.rb
@@ -1,1 +1,92 @@
-puts [1, 3].map(&:next)
+# tester for tic_tac_toe game
+
+require '../tic_tac_toe'
+require 'rspec/autorun'
+
+describe TicTacToe do
+  context "player can win" do
+    it "first player win" do
+      first_player = "ruby"
+      second_player = "java"
+      input = [[1,2],[1,1],[2,2],[3,3],[3,2]]
+      winner = TicTacToe.new(first_player,second_player,input).call
+      expect(winner).to eq first_player
+    end
+
+    it "first player win when first row is X X X" do
+      first_player = "ruby"
+      second_player = "html"
+      input = [[1,1],[2,2],[3,1],[2,1],[1,3],[3,3],[1,2],[2,3]]
+      winner = TicTacToe.new(first_player,second_player,input).call
+      expect(winner).to eq first_player
+    end
+
+    it "first row is X X X but also input more unneccesery information" do
+      first_player = "ruby"
+      second_player = "html"
+      input = [[1,1],[2,2],[3,1],[2,1],[1,3],[1,3],[4,4],[2,2],[3,3],[1,2],[2,3]]
+      winner = TicTacToe.new(first_player,second_player,input).call
+      expect(winner).to eq first_player
+    end
+
+    it "second player win" do
+      first_player = "css"
+      second_player = "ruby"
+      input = [[1,2],[1,1],[2,1],[1,3],[2,3],[2,2],[3,2],[3,1],[3,3]]
+      winner = TicTacToe.new(first_player,second_player,input).call
+      expect(winner).to eq second_player
+    end
+
+    it "second player win X X X type" do
+      first_player = "css"
+      second_player = "ruby"
+      input = [[2,2],[1,1],[3,3],[1,2],[3,1],[1,3],[2,3],[2,1]]
+      winner = TicTacToe.new(first_player,second_player,input).call
+      expect(winner).to eq second_player
+    end
+  end
+
+  context "no winner" do
+    it "match is draw" do
+      first_player = "c++"
+      second_player = "ruby"
+      input = [[1,1],[2,2],[3,3],[1,2],[1,3],[3,1],[3,2],[2,3],[2,1]]
+      winner = TicTacToe.new(first_player,second_player,input).call
+      expect(winner).to eq DRAW
+    end
+
+    it "not enough values" do
+      first_player = "c++"
+      second_player = "ruby"
+      input = [[1,1],[2,2],[3,3],[4,4]]
+      winner = TicTacToe.new(first_player,second_player,input).call
+      expect(winner).to eq NOT_ENOUGH_VALUES
+    end
+
+    it "no cordinates" do
+      first_player = "javascript"
+      second_player = "ruby"
+      input = []
+      winner = TicTacToe.new(first_player,second_player,input).call
+      expect(winner).to eq NOT_ENOUGH_VALUES
+    end
+
+    it "one cordinates" do
+      first_player = "javascript"
+      second_player = "ruby"
+      input = [[1,2]]
+      winner = TicTacToe.new(first_player,second_player,input).call
+      expect(winner).to eq NOT_ENOUGH_VALUES
+    end
+
+    it "input cordinate is not validate" do
+      first_player = "javascript"
+      second_player = "ruby"
+      input = ["kjan",['shbj',1], ['cbja', 'hjbbd'], [1, 'ss'],[1,1], ['dsd',[1]]]
+      winner = TicTacToe.new(first_player,second_player,input).call
+      expect(winner).to eq NOT_ENOUGH_VALUES
+    end
+  end
+end
+
+
--- week6_week7/tic_tac_toe/tic_tac_toe.rb
+++ week6_week7/tic_tac_toe/tic_tac_toe.rb
@@ -1,23 +1,32 @@
 # tic_tac_toe game
 
+CHARS = ['X', 'O'].freeze
 SIZE_RANGE = (1..3).freeze
 CORD_RANGE = (0..2).to_a.freeze
 TWO_CORD = 2
 MAX_CORD = 2
-MOD = 2
+MODULE = 2
+MATRIX_SIZE = 9
+ZERO = 0
 DECREASE = Proc.new { |element| element -= 1 }
+EMPTY_PLACE = '_'
+DRAW = "this is draw"
+NOT_ENOUGH_VALUES = "values must not be enough for find winner"
+NOT_IN_RANGE_OR_INTEGER = "input values must be in 1..3 range and integer"
+NOT_IN_RANGE = "input values must be in 1..3 range"
+MARKED_PLACE = "this cordinate is already taken"
 
-class Tic_Tac_Toe
+class TicTacToe
   attr_reader :board, :cordinates, :current_index, :free_space, :chars, :players
 
-  def initialize(first_player, second_player, cordinates = [])
+  def initialize(first_player, second_player, cordinates = nil)
     @players = [first_player,second_player]
     @cordinates = cordinates
-    @current_index = 0
-    @free_space = 9
+    @current_index = ZERO
+    @current_cord = [ZERO,ZERO]
+    @free_space = MATRIX_SIZE
     @winner_person = nil
-    @boolean = false
-    @chars = ['X', 'O']
+    @online_game = false
     @board = [
       ['_','_', '_'],
       ['_','_', '_'],
@@ -26,8 +35,8 @@ class Tic_Tac_Toe
   end
 
   def call
-    if cordinates.empty?
-      @boolean = true
+    if cordinates.nil?
+      @online_game = true
     end
     start_game
   end
@@ -35,78 +44,99 @@ class Tic_Tac_Toe
   private 
 
   def start_game
-    while free_space > 0
+    while free_space.positive?
       player_turn
       if @winner_person
         return @winner_person
       end
     end
+    if @online_game
+      puts DRAW
+    else
+      DRAW
+    end
   end
 
   def player_turn
-    if @boolean
+    if @online_game
       puts "it's #{players[current_index]} turn"
-      cord = gets.chomp().split(' ')
+      @current_cord = gets.chomp().split(' ')
     elsif @cordinates.empty?
-      return @winner_person = "values must not be enough for find winner"
+      return @winner_person = NOT_ENOUGH_VALUES
     else
-      cord = @cordinates.shift
+      @current_cord = @cordinates.shift
     end
-    puts cord
-    bool = is_not_integer?(cord)
-    cord.map!(&:to_i)
+
+    bool = is_not_integer?
 
     if bool 
-      puts "input values must be in 1..3 range and integer"
-    elsif cord.size != TWO_CORD
+      puts NOT_IN_RANGE_OR_INTEGER
+    elsif @current_cord.size != TWO_CORD
       puts "#{players[current_index]} input two index"
-    elsif cordinate_out_of_range?(cord)
-      puts "input values must be in 1..3 range"
-    elsif place_is_already_taken?(cord.map(&DECREASE))
-      puts "this cordinate is already taken"
+    elsif cordinate_out_of_range?
+      puts NOT_IN_RANGE
+    elsif place_is_already_taken?
+      puts MARKED_PLACE
     else
-      fill_board(cord.map(&DECREASE))
+      @current_cord.map!(&DECREASE)
+      fill_board
     end
   end
 
-  def fill_board(cord)
-    board[cord.first][cord.last] = chars[current_index]
+  def fill_board
+    board[@current_cord.first][@current_cord.last] = CHARS[current_index]
     @free_space -= 1
     board.each { |element| p element }
-    check_winner(cord)
-    @current_index = @current_index.next % MOD
+    check_winner
+    @current_index = @current_index.next % MODULE
   end
 
-  def is_not_integer?(cord)
-    cord.any? { |element| element.to_i.to_s != element.to_s }
+  def is_not_integer?
+    return true unless (@current_cord.is_a? Array)
+
+    if @current_cord.all? { |element| element.respond_to?(:to_i) }
+      @current_cord.map!(&:to_i)
+      @current_cord.any? { |element| element.to_i.to_s != element.to_s }
+    else
+      true
+    end
   end
 
-  def place_is_already_taken?(cord)
-    board[cord.first][cord.last] != '_'
+  def place_is_already_taken?
+    cord = @current_cord.map(&DECREASE)
+    board[cord.first][cord.last] != EMPTY_PLACE
   end
 
-  def cordinate_out_of_range?(cord)
-    (!SIZE_RANGE.include? cord.first) || (!SIZE_RANGE.include? cord.last)
+  def cordinate_out_of_range?
+     is_first_cordinate_bad? || is_second_cordiate_bad?
   end
 
-  def check_row(index)
-    board[index].all? {|element| element == chars[current_index]}
+  def is_first_cordinate_bad?
+    !SIZE_RANGE.include? @current_cord.first
   end
 
-  def check_column(index)
-    board.transpose[index].all? { |element| element == chars[current_index] }
+  def is_second_cordiate_bad?
+    !SIZE_RANGE.include? @current_cord.last
+  end
+
+  def check_row
+    board[@current_cord.first].all? {|element| element == CHARS[current_index]}
+  end
+
+  def check_column
+    board.transpose[@current_cord.last].all? { |element| element == CHARS[current_index] }
   end
 
   def left_diagonal
-    CORD_RANGE.all? { |index| board[index][index] == chars[current_index] }
+    CORD_RANGE.all? { |index| board[index][index] == CHARS[current_index] }
   end
 
   def right_diagonal
-    CORD_RANGE.all? { |index| board[index][MAX_CORD - index] == chars[current_index] }
+    CORD_RANGE.all? { |index| board[index][MAX_CORD - index] == CHARS[current_index] }
   end
 
-  def check_winner(cord)
-    if check_row(cord.first) || check_column(cord.last) 
+  def check_winner
+    if check_row || check_column 
       winner
     elsif left_diagonal || right_diagonal
       winner
@@ -116,7 +146,7 @@ class Tic_Tac_Toe
   end
 
   def winner
-    if @boolean
+    if @online_game
       puts "#{players[current_index]} is winner"
       exit 1
     else
@@ -130,8 +160,8 @@ end
 # puts "input second player name"
 # second_player = gets.chomp
 
-# #array = [[1,2],[1,1],[2,2],[3,3],[3,2]]
-# array = []
-# puts Tic_Tac_Toe.new(first_player, second_player,array).call
+# array = [[1,2],[1,1],[2,2],[3,3],[3,2]]
+
+# puts TicTacToe.new(first_player, second_player).call
 
 
